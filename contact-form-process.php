3903c0998624f0cda4c364ee81c09523566481d2
“Samuel <SamScott.Sarantos@calbaptist.edu>
2023-01-29 21:18:07 -0800

    now fixed lol

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,8 +2,8 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "New form submissions";
+    $email_to = "sam.sarantos@gmail.com";
+    $email_subject = "Submission from website";
 
     function problem($error)
     {
